4eb166b1da64aea422e4ed6767d94bf7019b2c7a
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-07 17:17:32 -0600

    fixed codegrade issues

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 export default class Todo extends React.Component {
+  constructor() {
+    super();
+    this.state = ""
+  }
 
   handleClick = () => {
     this.props.handleToggle(this.props.todo.id);
   }
 
   render() {
-    return (<li onClick={this.handleClick}>{this.props.todo.name} { this.props.todo.completed?<span>- completed</span> : <span></span>}</li>)
+    return (
+      <div>{this.props.todo && <li onClick={this.handleClick}>{this.props.todo.name} { this.props.todo.completed?<span>- completed</span> : <span></span>}</li>}</div>)
   }
 }
 
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -3,11 +3,15 @@ import Todo from './Todo';
 
 
 export default class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = ""
+  }
   render() {
     return (
          <ul>
           { 
-            this.props.todos.map(todo => {
+            this.props.todos && this.props.todos.map(todo => {
               return (<Todo key={todo.id} handleToggle={this.props.handleToggle} todo={todo}/>)
             })
           }
